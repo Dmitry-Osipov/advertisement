8e7dc1b8f09af1eb2618d67815fdfa71da07d2de
Dmitry Osipov <dimaosipov00@gmail.com>
2024-06-05 19:31:14 +0300

    Removed unused imports

--- domain/src/main/java/rf/senla/domain/dto/UserDto.java
+++ domain/src/main/java/rf/senla/domain/dto/UserDto.java
@@ -3,7 +3,6 @@ package rf.senla.domain.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
--- domain/src/main/java/rf/senla/domain/repository/UserRepository.java
+++ domain/src/main/java/rf/senla/domain/repository/UserRepository.java
@@ -1,8 +1,6 @@
 package rf.senla.domain.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rf.senla.domain.entity.User;
 
