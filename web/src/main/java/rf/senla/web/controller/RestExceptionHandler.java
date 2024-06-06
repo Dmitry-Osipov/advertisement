06e4ef24047775f01871ed20c1e35d12c337993f
Dmitry Osipov <dimaosipov00@gmail.com>
2024-06-06 15:30:14 +0300

    Changed http response status with exceptions

--- web/src/main/java/rf/senla/web/controller/RestExceptionHandler.java
+++ web/src/main/java/rf/senla/web/controller/RestExceptionHandler.java
@@ -1,9 +1,11 @@
 package rf.senla.web.controller;
 
+import jakarta.validation.ConstraintViolationException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -29,46 +31,6 @@ import java.time.LocalDateTime;
 @RestControllerAdvice
 public class RestExceptionHandler {
     /**
-     * Метод возвращает информацию об ошибке типа {@link NoEntityException} или {@link UsernameNotFoundException}
-     * @param ex ошибка
-     * @param request запрос
-     * @return информация об ошибке
-     */
-    @ExceptionHandler({NoEntityException.class, UsernameNotFoundException.class})
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorDto entityNotFoundException(Exception ex, WebRequest request) {
-        log.error("Ошибка отсутствия сущности - {}", ex.getMessage());
-        return getErrorDto(ex.getMessage(), request);
-    }
-
-    /**
-     * Метод возвращает информацию об ошибке типа {@link EntityContainedException} или
-     * {@link InvalidDataAccessApiUsageException}
-     * @param ex ошибка
-     * @param request запрос
-     * @return информация об ошибке
-     */
-    @ExceptionHandler({EntityContainedException.class, InvalidDataAccessApiUsageException.class})
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorDto entityContainedException(Exception ex, WebRequest request) {
-        log.error("Ошибка существования сущности - {}", ex.getMessage());
-        return getErrorDto(ex.getMessage(), request);
-    }
-
-    /**
-     * Метод возвращает информацию об ошибке типа {@link AccessDeniedException}
-     * @param ex ошибка
-     * @param request запрос
-     * @return информация об ошибке
-     */
-    @ExceptionHandler(AccessDeniedException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ErrorDto accessDeniedException(Exception ex, WebRequest request) {
-        log.error("Ошибка прав доступа - {}", ex.getMessage());
-        return getErrorDto(ex.getMessage(), request);
-    }
-
-    /**
      * Метод возвращает информацию об ошибке типа {@link NoResourceFoundException}
      * @param ex ошибка
      * @param request запрос
@@ -87,11 +49,51 @@ public class RestExceptionHandler {
      * @return информация об ошибке
      */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
     public ErrorDto httpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException ex,
                                                            WebRequest request) {
         log.error("Ошибка выбора http метода - {}", ex.getMessage());
         return getErrorDto(ErrorMessage.INCORRECT_HTTP_METHOD_SELECTED.getMessage(), request);
+    }
+
+    /**
+     * Метод возвращает информацию об ошибке типа {@link NoEntityException} или {@link UsernameNotFoundException}
+     * @param ex ошибка
+     * @param request запрос
+     * @return информация об ошибке
+     */
+    @ExceptionHandler({NoEntityException.class, UsernameNotFoundException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto entityNotFoundException(Exception ex, WebRequest request) {
+        log.error("Ошибка отсутствия сущности - {}", ex.getMessage());
+        return getErrorDto(ex.getMessage(), request);
+    }
+
+    /**
+     * Метод возвращает информацию об ошибке типа {@link EntityContainedException} или
+     * {@link InvalidDataAccessApiUsageException}
+     * @param ex ошибка
+     * @param request запрос
+     * @return информация об ошибке
+     */
+    @ExceptionHandler({EntityContainedException.class, InvalidDataAccessApiUsageException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto entityContainedException(Exception ex, WebRequest request) {
+        log.error("Ошибка существования сущности - {}", ex.getMessage());
+        return getErrorDto(ex.getMessage(), request);
+    }
+
+    /**
+     * Метод возвращает информацию об ошибке типа {@link AccessDeniedException}
+     * @param ex ошибка
+     * @param request запрос
+     * @return информация об ошибке
+     */
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto accessDeniedException(Exception ex, WebRequest request) {
+        log.error("Ошибка прав доступа - {}", ex.getMessage());
+        return getErrorDto(ex.getMessage(), request);
     }
 
     /**
@@ -101,7 +103,7 @@ public class RestExceptionHandler {
      * @return информация об ошибке
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto methodArgumentNotValidException(Exception ex, WebRequest request) {
         log.error("Ошибка валидации аргумента - {}", ex.getMessage());
         return getErrorDto(ex.getMessage(), request);
@@ -114,7 +116,7 @@ public class RestExceptionHandler {
      * @return информация об ошибке
      */
     @ExceptionHandler(TechnicalException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto technicalException(TechnicalException ex, WebRequest request) {
         log.error("Техническая ошибка - {}", ex.getMessage());
         return getErrorDto(ex.getMessage(), request);
@@ -127,9 +129,35 @@ public class RestExceptionHandler {
      * @return информация об ошибке
      */
     @ExceptionHandler(ResetPasswordTokenException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorDto resetPasswordTokenException(ResetPasswordTokenException ex, WebRequest request) {
         log.error("Ошибка валидации токена восстановления пароля - {}", ex.getMessage());
+        return getErrorDto(ex.getMessage(), request);
+    }
+
+    /**
+     * Метод возвращает информацию об ошибке типа {@link ConstraintViolationException}
+     * @param ex ошибка
+     * @param request запрос
+     * @return информация об ошибке
+     */
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto constraintViolation(Exception ex, WebRequest request) {
+        log.error("Ошибка валидации jakarta {}", ex.getMessage());
+        return getErrorDto(ex.getMessage(), request);
+    }
+
+    /**
+     * Метод возвращает информацию об ошибке типа {@link BadCredentialsException}
+     * @param ex ошибка
+     * @param request запрос
+     * @return информация об ошибке
+     */
+    @ExceptionHandler(BadCredentialsException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDto badCredentials(Exception ex, WebRequest request) {
+        log.error("Ошибка анкетных данны {}", ex.getMessage());
         return getErrorDto(ex.getMessage(), request);
     }
 
@@ -140,7 +168,7 @@ public class RestExceptionHandler {
      * @return информация об ошибке
      */
     @ExceptionHandler(EmailException.class)
-    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ErrorDto emailException(EmailException ex, WebRequest request) {
         log.error("Ошибка при работе с почтой - {}", ex.getMessage());
         return getErrorDto(ex.getMessage(), request);
