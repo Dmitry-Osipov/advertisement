eb2ec55916aeeb47b50039e0b0707599dbe19db3
Dmitry Osipov <dimaosipov00@gmail.com>
2024-06-06 21:21:03 +0300

    Added tests to AdvertisementRepository

--- web/src/test/java/rf/senla/web/repository/AdvertisementRepositoryTest.java
+++ web/src/test/java/rf/senla/web/repository/AdvertisementRepositoryTest.java
@@ -4,8 +4,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.domain.Pageable;
+import rf.senla.domain.entity.Advertisement;
+import rf.senla.domain.entity.AdvertisementStatus;
+import rf.senla.domain.entity.User;
 import rf.senla.domain.repository.AdvertisementRepository;
 import rf.senla.domain.repository.UserRepository;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -17,6 +26,119 @@ class AdvertisementRepositoryTest {
 
     @Test
     void saveDoesNotThrowException() {
-        // TODO: continue
+        User owner = userRepository.findByUsername("user123").orElseThrow();
+        Advertisement expected = Advertisement.builder()
+                .user(owner)
+                .headline("Test Headline")
+                .description("Test description")
+                .price(0)
+                .status(AdvertisementStatus.REVIEW)
+                .build();
+
+        Advertisement actual = assertDoesNotThrow(() -> sut.save(expected));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void findAllWithActiveStatusReturnsCorrectData() {
+        Advertisement advertisement = Advertisement.builder()
+                .user(userRepository.findByUsername("user123").orElseThrow())
+                .headline("Test Headline")
+                .description("Test description")
+                .price(0)
+                .status(AdvertisementStatus.REVIEW)
+                .build();
+        sut.save(advertisement);
+        Pageable pageable = Pageable.ofSize(20);
+        int size = 10;
+
+        List<Advertisement> actual = assertDoesNotThrow(() -> sut.findAllWithActiveStatus(pageable));
+
+        assertEquals(size, actual.size());
+        assert actual.stream().allMatch(ad -> ad.getStatus().equals(AdvertisementStatus.ACTIVE));
+    }
+
+    @Test
+    void findByPriceBetweenWithActiveStatusReturnsCorrectData() {
+        Pageable pageable = Pageable.ofSize(20);
+        int minPrice = 5000;
+        int masPrice = 7000;
+        int size = 3;
+
+        List<Advertisement> actual = assertDoesNotThrow(() ->
+                sut.findByPriceBetweenWithActiveStatus(minPrice, masPrice, pageable));
+
+        assertEquals(size, actual.size());
+        assert actual.stream().allMatch(ad -> ad.getStatus().equals(AdvertisementStatus.ACTIVE));
+    }
+
+    @Test
+    void findByPriceBetweenAndHeadlineIgnoreCaseWithActiveStatusReturnsCorrectData() {
+        Pageable pageable = Pageable.ofSize(20);
+        String headline = "Smartphone";
+        int minPrice = 0;
+        int maxPrice = 10_000;
+        Advertisement advertisement = Advertisement.builder()
+                .user(userRepository.findByUsername("user123").orElseThrow())
+                .headline(headline)
+                .description("Test description")
+                .price(0)
+                .status(AdvertisementStatus.REVIEW)
+                .build();
+        sut.save(advertisement);
+        int size = 1;
+
+        List<Advertisement> actual = assertDoesNotThrow(() ->
+                sut.findByPriceBetweenAndHeadlineIgnoreCaseWithActiveStatus(minPrice, maxPrice, headline, pageable));
+
+        assertEquals(size, actual.size());
+        assert actual.stream().allMatch(ad -> ad.getStatus().equals(AdvertisementStatus.ACTIVE));
+    }
+
+    @Test
+    void findByUserReturnsCorrectData() {
+        Pageable pageable = Pageable.ofSize(20);
+        User user = userRepository.findByUsername("user123").orElseThrow();
+        Advertisement advertisement = Advertisement.builder()
+                .user(user)
+                .headline("Test headline")
+                .description("Test description")
+                .price(0)
+                .status(AdvertisementStatus.REVIEW)
+                .build();
+        sut.save(advertisement);
+        int size = 2;
+
+        List<Advertisement> actual = assertDoesNotThrow(() -> sut.findByUser(user, pageable));
+
+        assertEquals(size, actual.size());
+    }
+
+    @Test
+    void findByUserWithActiveStatusReturnsCorrectData() {
+        Pageable pageable = Pageable.ofSize(20);
+        User user = userRepository.findByUsername("user123").orElseThrow();
+        Advertisement advertisement = Advertisement.builder()
+                .user(user)
+                .headline("Test headline")
+                .description("Test description")
+                .price(0)
+                .status(AdvertisementStatus.REVIEW)
+                .build();
+        sut.save(advertisement);
+        int size = 1;
+
+        List<Advertisement> actual = assertDoesNotThrow(() -> sut.findByUserWithActiveStatus(user, pageable));
+
+        assertEquals(size, actual.size());
+        assert actual.stream().allMatch(ad -> ad.getStatus().equals(AdvertisementStatus.ACTIVE));
+    }
+
+    @Test
+    void deleteByUserIdDoesNotThrowException() {
+        User user = userRepository.findByUsername("user123").orElseThrow();
+
+        assertDoesNotThrow(() -> sut.deleteByUser_Id(user.getId()));
     }
 }
