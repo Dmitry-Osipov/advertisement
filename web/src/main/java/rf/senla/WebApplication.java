1d22637932a63df81308724a2efd9fbbe8f3f7a2
Dmitry Osipov <dimaosipov00@gmail.com>
2024-06-08 20:55:54 +0300

    Added javadoc

--- web/src/main/java/rf/senla/WebApplication.java
+++ web/src/main/java/rf/senla/WebApplication.java
@@ -3,8 +3,17 @@ package rf.senla;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Главный класс для веб-приложения Advertisement.
+ * Этот класс служит точкой входа для Spring Boot приложения. Он содержит метод {@link #main(String[])},
+ * который запускает приложение.
+ */
 @SpringBootApplication
 public class WebApplication {
+    /**
+     * Главный метод, запускающий приложение.
+     * @param args аргументы командной строки
+     */
     public static void main(String[] args) {
         SpringApplication.run(WebApplication.class, args);
     }
